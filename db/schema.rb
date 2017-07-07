bd73675ac6884b04823b58fda17de2dddacf37d5
nurami <mrnursil@gmail.com>
2017-07-07 15:34:42 +0300

    add schema for skill

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707102652) do
+ActiveRecord::Schema.define(version: 20170707120535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
