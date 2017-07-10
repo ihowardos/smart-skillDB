592a4200b2ffe458522944165677396fc4ed980a
nurami <mrnursil@gmail.com>
2017-07-10 20:05:58 +0300

    add model UsersSkill and relations many to many

---
+++ app/models/users_skill.rb
@@ -0,0 +1,4 @@
+class UsersSkill < ApplicationRecord
+  belongs_to :skill
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710090251) do
+ActiveRecord::Schema.define(version: 20170710165816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170710090251) do
   create_table "skills", force: :cascade do |t|
     t.string   "name"
     t.integer  "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users_skills", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "skill_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
