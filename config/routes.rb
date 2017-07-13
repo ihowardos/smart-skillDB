2ace5feb80f122f7941682bed7d81a23dc48dca0
Sergey Antonov <asergey96@gmail.com>
2017-07-13 15:25:31 +0300

    изменен маршрут к профилю юзера

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :skills
   resources :users_skills
-  
+  resources :profile, only: [:show]
+
   devise_for :users, controllers: { registrations: "users_controllers/registrations" }
 
-  get 'users/:id', to: 'profiles#show', as: 'users'
   get '/:page', to: 'pages#show', as: :page
 
   root 'pages#show',   page: 'home'
