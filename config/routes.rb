42469be0f11a4f165d6aff8bdfd11b2e7bf16a36
Sergey Antonov <asergey96@gmail.com>
2017-07-18 18:06:07 +0300

    added view's style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_skills
-  resources :skills
+  resources :user_skills, except:[:new, :edit]
+  resources :skills, only:[:create, :destroy, :index]
   resources :profile, only: [:show]
-  resources :databases
+  resources :databases, only: [:index]
 
   devise_for :users, controllers: { registrations: "users_controllers/registrations" }
 
